4c1d613a9d66aba7a30fa268255e346d5cc56eb6
Fil <shinymetal6@gmail.com>
2024-03-18 14:54:46 +0100

    added xmodem and modbus to fy

--- STM32H743/Fyberloom/process_2.c
+++ STM32H743/Fyberloom/process_2.c
@@ -10,21 +10,87 @@
 
 #ifdef	FY_201023_01
 
+//#define	USE_XMODEM
+#define	USE_MODBUS
+
+#ifdef	USE_XMODEM
+
+XMODEM_DATA_AREA	uint8_t	xmodem_data_area[131072];
+
 void process_2(uint32_t process_id)
 {
 uint32_t	wakeup,flags;
-	create_timer(TIMER_ID_0,400,TIMERFLAGS_FOREVER | TIMERFLAGS_ENABLED);
+	allocate_hw(HW_UART1,0);
+	xmodem_init(HW_UART1,(uint8_t *)xmodem_data_area);
+	create_timer(TIMER_ID_0,100,TIMERFLAGS_FOREVER | TIMERFLAGS_ENABLED);
 	while(1)
 	{
-		wait_event(EVENT_TIMER);
+		wait_event(EVENT_TIMER | EVENT_UART1_IRQ);
 		get_wakeup_flags(&wakeup,&flags);
 
 		if (( wakeup & WAKEUP_FROM_TIMER) == WAKEUP_FROM_TIMER)
 		{
-			wakeup=0;
+			xmodem_process(wakeup);
 		}
+		if (( wakeup & WAKEUP_FROM_UART1_IRQ) == WAKEUP_FROM_UART1_IRQ)
+		{
+			if (( flags & WAKEUP_FLAGS_UART_RX) == WAKEUP_FLAGS_UART_RX )
+			{
+				HAL_GPIO_TogglePin(LED_1_GPIOPORT, LED_1_GPIOBIT);
+				xmodem_process(wakeup);
+			}
+		}
+	}
+}
+#endif
+#ifdef	USE_MODBUS
+#define	LOCAL_MODBUS_BUFFERLEN		513
+XMODEM_DATA_AREA	uint8_t 	modbus_rx_buf[LOCAL_MODBUS_BUFFERLEN];
+#define	LOCAL_MODBUS_ADDRESS		1
 
+void process_2(uint32_t process_id)
+{
+uint32_t	wakeup,flags,modbus_error=0;
+	allocate_hw(HW_UART1,0);
+	if ( modbus_init(HW_UART1,LOCAL_MODBUS_ADDRESS,modbus_rx_buf,LOCAL_MODBUS_BUFFERLEN) != 0 )
+		modbus_error=1;
+
+	create_timer(TIMER_ID_0,100,TIMERFLAGS_FOREVER | TIMERFLAGS_ENABLED);
+	while(1)
+	{
+		wait_event(EVENT_TIMER | EVENT_UART3_IRQ);
+		get_wakeup_flags(&wakeup,&flags);
+
+		if (( wakeup & WAKEUP_FROM_TIMER) == WAKEUP_FROM_TIMER)
+		{
+			if ( modbus_error == 0 )
+			{
+				HAL_GPIO_TogglePin(LED_1_GPIOPORT, LED_1_GPIOBIT);
+				/*
+				if ( modbus_get_coil(0) )
+					HAL_GPIO_WritePin(LED_1_GPIOPORT, LED_1_GPIOBIT,GPIO_PIN_SET);
+				else
+					HAL_GPIO_WritePin(LED_1_GPIOPORT, LED_1_GPIOBIT,GPIO_PIN_RESET);
+				if ( modbus_get_coil(1) )
+					HAL_GPIO_WritePin(LED_2_GPIOPORT, LED_2_GPIOBIT,GPIO_PIN_SET);
+				else
+					HAL_GPIO_WritePin(LED_2_GPIOPORT, LED_2_GPIOBIT,GPIO_PIN_RESET);
+				*/
+			}
+		}
+		if (( wakeup & WAKEUP_FROM_UART1_IRQ) == WAKEUP_FROM_UART1_IRQ)
+		{
+			if (( flags & WAKEUP_FLAGS_UART_RX) == WAKEUP_FLAGS_UART_RX )
+			{
+				if ( modbus_process() == 0)
+				{
+
+				}
+			}
+		}
 	}
 }
 
-#endif // #ifdef	STM32H743xx
+#endif //#define	USE_XMODEM or USE_MODBUS
+
+#endif // #ifdef	FY_201023_01
