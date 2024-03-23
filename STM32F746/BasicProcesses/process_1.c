b4419b84d6d167fa95b8d961f3916b304e445062
Fil <shinymetal6@gmail.com>
2024-03-23 08:16:26 +0100

    added F746GDISCO

---
+++ STM32F746/BasicProcesses/process_1.c
@@ -0,0 +1,44 @@
+/* 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Project : A_os
+*/
+/*
+ * process_1.c
+ *
+ *  Created on: Mar 22, 2024
+ *      Author: fil
+ */
+
+#include "main.h"
+#include "A_os_includes.h"
+
+#ifdef	STM32F746xx
+
+void process_1(uint32_t process_id)
+{
+uint32_t	wakeup,flags;
+
+	create_timer(TIMER_ID_0,500,TIMERFLAGS_FOREVER | TIMERFLAGS_ENABLED);
+	while(1)
+	{
+		wait_event(EVENT_TIMER );
+		get_wakeup_flags(&wakeup,&flags);
+
+		if (( wakeup & WAKEUP_FROM_TIMER) == WAKEUP_FROM_TIMER)
+		{
+		}
+	}
+}
+
+#endif //#ifdef	STM32F746xx
