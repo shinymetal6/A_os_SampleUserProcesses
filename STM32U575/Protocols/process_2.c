fe246ca9a02964402a5d84ec075c4b79fa8b6acc
Fil <shinymetal6@gmail.com>
2024-03-25 16:45:08 +0100

    added dfu sample

--- STM32U575/Protocols/process_2.c
+++ STM32U575/Protocols/process_2.c
@@ -12,13 +12,21 @@
 
 void process_2(uint32_t process_id)
 {
-uint32_t	wakeup,flags;
+uint32_t	wakeup,flags,counter = 0;
 	create_timer(TIMER_ID_0,1000,TIMERFLAGS_FOREVER | TIMERFLAGS_ENABLED);
 	while(1)
 	{
 		wait_event(EVENT_TIMER );
 		get_wakeup_flags(&wakeup,&flags);
-
+		if (( wakeup & WAKEUP_FROM_TIMER) == WAKEUP_FROM_TIMER)
+		{
+			counter++;
+			if ( counter > 4 )
+			{
+				HAL_GPIO_WritePin(LED_3_GPIOPORT, LED_3_GPIOBIT,GPIO_PIN_RESET);
+				//dfu_run_bootloader();
+			}
+		}
 	}
 }
 
